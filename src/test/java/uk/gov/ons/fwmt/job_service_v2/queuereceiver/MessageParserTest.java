e76e5ad9e24c0b8b4052e0eae8a67f9a4b84630e
jacobharrison <jacob.harrison@qa.com>
2018-08-21 15:06:08 +0100

    clean up

--- src/test/java/uk/gov/ons/fwmt/job_service_v2/queuereceiver/MessageParserTest.java
+++ src/test/java/uk/gov/ons/fwmt/job_service_v2/queuereceiver/MessageParserTest.java
@@ -25,83 +25,83 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class MessageParserTest {
 
-    @InjectMocks
-    MessageParser messageParser;
+  @InjectMocks
+  MessageParser messageParser;
 
-    @Mock
-    private TMJobConverterService tmJobConverterService;
+  @Mock
+  private TMJobConverterService tmJobConverterService;
 
-    @Mock
-    private TMService tmService;
+  @Mock
+  private TMService tmService;
 
-    @Mock
-    private ObjectMapper mapper;
+  @Mock
+  private ObjectMapper mapper;
 
-    @Test
-    public void receiveMessageCreate() throws InstantiationException, IllegalAccessException, IOException {
-        JSONObject json = new JSONObject();
-        JSONObject address = new JSONObject();
-        json.put("actionType", "Create");
-        json.put("jobIdentity", "1234");
-        json.put("surveyType", "HH");
-        json.put("preallocatedJob", "true");
-        json.put("mandatoryResourceAuthNo", "1234");
-        json.put("dueDate", "20180216");
-        address.put("line1", "886");
-        address.put("line2", "Prairie Rose");
-        address.put("line3", "Trail");
-        address.put("line4", "RU");
-        address.put("townName", "Borodinskiy");
-        address.put("postCode", "188961");
-        address.put("latitude", "61.7921776");
-        address.put("longitude", "34.3739957");
-        json.put("address", address);
+  @Test
+  public void receiveMessageCreate() throws InstantiationException, IllegalAccessException, IOException {
+    JSONObject json = new JSONObject();
+    JSONObject address = new JSONObject();
+    json.put("actionType", "Create");
+    json.put("jobIdentity", "1234");
+    json.put("surveyType", "HH");
+    json.put("preallocatedJob", "true");
+    json.put("mandatoryResourceAuthNo", "1234");
+    json.put("dueDate", "20180216");
+    address.put("line1", "886");
+    address.put("line2", "Prairie Rose");
+    address.put("line3", "Trail");
+    address.put("line4", "RU");
+    address.put("townName", "Borodinskiy");
+    address.put("postCode", "188961");
+    address.put("latitude", "61.7921776");
+    address.put("longitude", "34.3739957");
+    json.put("address", address);
 
-        String message;
-        message = json.toString();
+    String message;
+    message = json.toString();
 
-        FWMTCreateJobRequest expectedCreateJobRequest = new FWMTCreateJobRequest();
-        when(mapper.readValue(message, FWMTCreateJobRequest.class)).thenReturn(expectedCreateJobRequest);
+    FWMTCreateJobRequest expectedCreateJobRequest = new FWMTCreateJobRequest();
+    when(mapper.readValue(message, FWMTCreateJobRequest.class)).thenReturn(expectedCreateJobRequest);
 
-        SendCreateJobRequestMessage sendCreateJobRequest = new SendCreateJobRequestMessage();
-        when(tmJobConverterService.createJob(eq(expectedCreateJobRequest), eq(""))).thenReturn(sendCreateJobRequest);
+    SendCreateJobRequestMessage sendCreateJobRequest = new SendCreateJobRequestMessage();
+    when(tmJobConverterService.createJob(eq(expectedCreateJobRequest), eq(""))).thenReturn(sendCreateJobRequest);
 
-        messageParser.receiveMessage(message);
+    messageParser.receiveMessage(message);
 
-        System.out.println(json);
-        System.out.println(message);
+    System.out.println(json);
+    System.out.println(message);
 
-        Mockito.verify(tmJobConverterService).createJob(any(), any());
-        Mockito.verify(tmJobConverterService, never()).deleteJob(any(), any());
-        Mockito.verify(tmService).send(any());
-    }
+    Mockito.verify(tmJobConverterService).createJob(any(), any());
+    Mockito.verify(tmJobConverterService, never()).deleteJob(any(), any());
+    Mockito.verify(tmService).send(any());
+  }
 
-    @Test
-    public void receiveMessageCancel() throws InstantiationException, IllegalAccessException, IOException {
-        JSONObject json = new JSONObject();
-        json.put("actionType", "Cancel");
-        json.put("jobIdentity", "1234");
-        json.put("reason", "incorrect address");
+  @Test
+  public void receiveMessageCancel() throws InstantiationException, IllegalAccessException, IOException {
+    JSONObject json = new JSONObject();
+    json.put("actionType", "Cancel");
+    json.put("jobIdentity", "1234");
+    json.put("reason", "incorrect address");
 
-        String message;
-        message = json.toString();
+    String message;
+    message = json.toString();
 
-        FWMTCancelJobRequest expectedCancelJobRequest = new FWMTCancelJobRequest();
-        when(mapper.readValue(message, FWMTCancelJobRequest.class)).thenReturn(expectedCancelJobRequest);
+    FWMTCancelJobRequest expectedCancelJobRequest = new FWMTCancelJobRequest();
+    when(mapper.readValue(message, FWMTCancelJobRequest.class)).thenReturn(expectedCancelJobRequest);
 
-        SendDeleteJobRequestMessage sendDeleteJobRequest = new SendDeleteJobRequestMessage();
-        when(tmJobConverterService
-            .deleteJob(eq(expectedCancelJobRequest.getJobIdentity()), eq(expectedCancelJobRequest.getReason())))
-            .thenReturn(sendDeleteJobRequest);
+    SendDeleteJobRequestMessage sendDeleteJobRequest = new SendDeleteJobRequestMessage();
+    when(tmJobConverterService
+        .deleteJob(eq(expectedCancelJobRequest.getJobIdentity()), eq(expectedCancelJobRequest.getReason())))
+        .thenReturn(sendDeleteJobRequest);
 
-        messageParser.receiveMessage(message);
+    messageParser.receiveMessage(message);
 
-        System.out.println(json);
-        System.out.println(message);
+    System.out.println(json);
+    System.out.println(message);
 
-        Mockito.verify(tmJobConverterService, never()).createJob(any(), any());
-        Mockito.verify(tmJobConverterService).deleteJob(any(), any());
-        Mockito.verify(tmService).send(any());
-    }
+    Mockito.verify(tmJobConverterService, never()).createJob(any(), any());
+    Mockito.verify(tmJobConverterService).deleteJob(any(), any());
+    Mockito.verify(tmService).send(any());
+  }
 
 }
