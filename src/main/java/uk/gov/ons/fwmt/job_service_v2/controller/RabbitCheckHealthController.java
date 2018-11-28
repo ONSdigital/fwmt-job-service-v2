c5e864218ec9b6b09e6871df02ce510f2c46f228
Simon <sdiaz1000@gmail.com>
2018-11-28 15:48:10 +0000

    fwmt-602 Fix potential null pointer issue

--- src/main/java/uk/gov/ons/fwmt/job_service_v2/controller/RabbitCheckHealthController.java
+++ src/main/java/uk/gov/ons/fwmt/job_service_v2/controller/RabbitCheckHealthController.java
@@ -1,6 +1,7 @@
 package uk.gov.ons.fwmt.job_service_v2.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.Properties;
+
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,11 +23,9 @@ public class RabbitCheckHealthController {
   @RequestMapping(value = "/queue", method = RequestMethod.GET, produces = "application/json")
   public boolean canAccessQueue(@RequestParam("qname") String qname) {
     RabbitAdmin rabbitAdmin = new RabbitAdmin(factory);
-    String result1 = rabbitAdmin.getQueueProperties(qname).getProperty("QUEUE_NAME");
-    if (qname.equals(result1)) {
-      return true;
-    }
-    return false;
+    
+    Properties queueProperties = rabbitAdmin.getQueueProperties(qname);
+    return (queueProperties!=null && qname.equals(queueProperties.getProperty("QUEUE_NAME")));
   }
 
 }
